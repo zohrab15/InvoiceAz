c847b44cf3fb9efd814b4897b38520b3e3bf69b3
mirza <mirza@example.com>
2026-02-18 20:03:58 +0100

    fix: enforce DEBUG=False on Render for proper static file serving

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -26,6 +26,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-!(yj$6&qa##p=p@(v!+k0
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'True') == 'True'
+if 'RENDER' in os.environ:
+    DEBUG = False
 
 import warnings
 warnings.filterwarnings('ignore', message='.*allauth.*deprecated.*')
