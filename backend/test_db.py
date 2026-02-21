617acbc22c43d0d89f1370970920fa26870471ca
mirza <mirza@example.com>
2026-02-21 18:29:06 +0100

    Cleanup diagnostic scripts

--- backend/test_db.py
+++
@@ -1,8 +0,0 @@
-import os
-import django
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
-django.setup()
-
-from clients.models import Client
-for c in Client.objects.all():
-    print(f"Client: {c.name}, Assigned To: {c.assigned_to_id}")
