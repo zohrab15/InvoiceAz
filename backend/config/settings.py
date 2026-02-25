2bf13181ec7d121377d9f0940cbc88cc5f1ecb76
mirza <mirza@example.com>
2026-02-25 16:08:00 +0100

    chore: fix django-allauth deprecation warnings

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -85,14 +85,11 @@ AUTHENTICATION_BACKENDS = [
 ]
 
 # Account Settings
-ACCOUNT_AUTHENTICATION_METHOD = 'email'
-ACCOUNT_EMAIL_REQUIRED = True
-ACCOUNT_USERNAME_REQUIRED = False
 ACCOUNT_LOGIN_METHODS = {'email'}
 ACCOUNT_EMAIL_VERIFICATION = 'optional' 
 ACCOUNT_CONFIRM_EMAIL_ON_GET = True
 ACCOUNT_UNIQUE_EMAIL = True
-ACCOUNT_SIGNUP_FIELDS = [] # Correctly override default to exclude username
+ACCOUNT_SIGNUP_FIELDS = ['email*', 'password1*', 'password2*']
 LOGIN_REDIRECT_URL = '/api/users/google/callback/'
 LOGIN_URL = 'account_login'
 ACCOUNT_LOGOUT_ON_GET = True
